1269365fbf63401644c60871146108794571d834
Robin Duhan <52245137+RobinDuhan@users.noreply.github.com>
2026-02-27 20:33:44 +0530

    INST-20578 - Fixing auto-upgrade for GCP and Azure

--- internal/autoupdate/validate.go
+++ internal/autoupdate/validate.go
@@ -25,80 +25,7 @@ import (
 	"os"
 	"os/exec"
 	"strings"
-
-	"gopkg.in/yaml.v3"
 )
-
-type EnvVar struct {
-	Name  string `json:"name"`
-	Value string `json:"value"`
-}
-
-type Provider struct {
-	Name                 string          `json:"name"`
-	MatchImages          []string        `json:"matchImages"`
-	DefaultCacheDuration string          `json:"defaultCacheDuration"`
-	ApiVersion           string          `json:"apiVersion"`
-	Env                  []EnvVar        `json:"env"`
-	TokenAttributes      TokenAttributes `json:"tokenAttributes,omitempty"`
-}
-
-type TokenAttributes struct {
-	ServiceAccountTokenAudience          string   `json:"serviceAccountTokenAudience,omitempty"`
-	CacheType                            string   `json:"cacheType,omitempty"`
-	RequireServiceAccount                bool     `json:"requireServiceAccount"`
-	RequiredServiceAccountAnnotationKeys []string `json:"requiredServiceAccountAnnotationKeys,omitempty"`
-	OptionalServiceAccountAnnotationKeys []string `json:"optionalServiceAccountAnnotationKeys,omitempty"`
-}
-
-type CredentialProviderConfig struct {
-	ApiVersion string     `json:"apiVersion"`
-	Kind       string     `json:"kind"`
-	Providers  []Provider `json:"providers"`
-}
-
-// loadProviderEnvsFromFile loads environment variables for a specific provider from a config file.
-func loadProviderEnvsFromFile(logs *logger.Logger, configPath string, targetProviderName string) ([]string, error) {
-	data, err := os.ReadFile(configPath)
-	if err != nil {
-		logs.Error("Error reading config file: " + err.Error())
-		return nil, err
-	}
-
-	var config CredentialProviderConfig
-
-	// if yaml, parse it in yaml
-	if strings.HasSuffix(configPath, ".yaml") || strings.HasSuffix(configPath, ".yml") {
-		if err := yaml.Unmarshal(data, &config); err != nil {
-			logs.Error("Error unmarshalling config YAML: " + err.Error())
-			return nil, err
-		}
-	} else {
-		// if json, parse it in json
-		if err := json.Unmarshal(data, &config); err != nil {
-			logs.Error("Error unmarshalling config JSON: " + err.Error())
-			return nil, err
-		}
-	}
-
-	var providerEnvs []string
-	for _, provider := range config.Providers {
-		if provider.Name == targetProviderName {
-			if provider.Env != nil {
-				for _, envVar := range provider.Env {
-					logs.Info("Found environment variable for provider " + targetProviderName + ": " + envVar.Name + "=" + envVar.Value)
-					providerEnvs = append(providerEnvs, fmt.Sprintf("%s=%s", envVar.Name, envVar.Value))
-				}
-			}
-			break
-		}
-	}
-
-	if len(providerEnvs) == 0 {
-		logs.Info("No specific environment variables found for provider " + targetProviderName + " in " + configPath + " or provider not found.")
-	}
-	return providerEnvs, nil
-}
 
 // createRequestJson creates a JSON request for the credential provider using the given Artifactory URL.
 func createRequestJson(logs *logger.Logger, artifactoryUrl string, request utils.CredentialProviderRequest) ([]byte, error) {
@@ -109,7 +36,9 @@ func createRequestJson(logs *logger.Logger, artifactoryUrl string, request utils
 		Image:      artifactoryUrl,
 	}
 
-	if request.ServiceAccountAnnotations["JFrogExchange"] == "true" && request.ServiceAccountAnnotations["eks.amazonaws.com/role-arn"] != "" {
+	if request.ServiceAccountAnnotations["JFrogExchange"] == "true" &&
+		(request.ServiceAccountAnnotations["eks.amazonaws.com/role-arn"] != "" ||
+			request.ServiceAccountAnnotations["azure.workload.identity/client-id"] != "") {
 		jsonReq.ServiceAccountToken = request.ServiceAccountToken
 		jsonReq.ServiceAccountAnnotations = request.ServiceAccountAnnotations
 	}
@@ -124,20 +53,14 @@ func createRequestJson(logs *logger.Logger, artifactoryUrl string, request utils
 }
 
 // fetchArtifactoryAuth runs the new binary with the given request and environment, returning the parsed response.
-func fetchArtifactoryAuth(ctx context.Context, client *http.Client, logs *logger.Logger, newBinaryPath string, kubeletPluginRequest []byte, kubeletProviderEnvs []string) (utils.CredentialProviderResponse, error) {
+func fetchArtifactoryAuth(ctx context.Context, client *http.Client, logs *logger.Logger, newBinaryPath string, kubeletPluginRequest []byte) (utils.CredentialProviderResponse, error) {
 	logs.Info("Validating new binary with request JSON: " + string(kubeletPluginRequest))
 
 	os.Chmod(newBinaryPath, 0755)
 	cmd := exec.Command(newBinaryPath)
 	cmd.Stdin = strings.NewReader(string(kubeletPluginRequest))
 
-	// Load environment variables from config file
-	// Combine current environment with provider-specific ones
-	// Provider-specific ones will override if there are duplicates from os.Environ()
 	cmd.Env = os.Environ()
-	if len(kubeletProviderEnvs) > 0 {
-		cmd.Env = append(cmd.Env, kubeletProviderEnvs...)
-	}
 	logs.Info("Validating new binary with following environment variables: " + strings.Join(cmd.Env, " "))
 
 	var stdoutBuf, stderrBuf bytes.Buffer
@@ -212,24 +135,17 @@ func validateKubeletBinary(ctx context.Context, request utils.CredentialProvider
 		return err
 	}
 
-	kubeletPluginConfigPath := utils.GetEnvs(logs, "KUBELET_PLUGIN_CONFIG_PATH", "/etc/eks/image-credential-provider/config.json")
-	targetProviderName := utils.GetEnvs(logs, "TARGET_PROVIDER_NAME", "jfrog-credential-provider")
 	artifactoryUrl := utils.GetEnvs(logs, "artifactory_url", "")
 	if artifactoryUrl == "" {
 		logs.Error("Error: artifactoryUrl environment variable is not set.")
 		return fmt.Errorf("artifactoryUrl environment variable is not set")
 	}
 
-	providerEnvs, err := loadProviderEnvsFromFile(logs, kubeletPluginConfigPath, targetProviderName)
-	if err != nil {
-		return err
-	}
-
 	providerRequest, err := createRequestJson(logs, artifactoryUrl, request)
 	if err != nil {
 		return err
 	}
-	providerResponse, err := fetchArtifactoryAuth(ctx, client, logs, newBinaryPath, providerRequest, providerEnvs)
+	providerResponse, err := fetchArtifactoryAuth(ctx, client, logs, newBinaryPath, providerRequest)
 	if err != nil {
 		return err
 	}
